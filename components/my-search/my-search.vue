4c928fd34ad21afee430c3e42e2ff92f1074e551
Holy-Mortal <holy_mortal@163.com>
2021-07-30 21:09:00 +0800

    搜索功能开发完毕

---
+++ components/my-search/my-search.vue
@@ -0,0 +1,61 @@
+<template>
+  <view class="my-search-container" :style="{'background-color' : bgcolor}" @click="searchBoxHandler">
+    <view class="my-search-box" :style="{'border-radius' : radius + 'px'}">
+      <!-- 使用 uni-ui 提供的图标组件 -->
+      <uni-icons type="search" size="17"></uni-icons>
+      <text class="placeholder">搜索</text>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name:"my-search",
+    data() {
+      return {
+        
+      };
+    },
+    methods: {
+      searchBoxHandler() {
+        console.log('=======');
+        this.$emit('click')
+      }
+    },
+    props: {
+      // 背景颜色
+      bgcolor: {
+        type: String,
+        default: '#c00000'
+      },
+      // 圆角尺寸
+      radius: {
+        type: Number,
+        default: 18 // px
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+.my-search-container {
+  height: 50px;
+  // background-color: #c00000;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  .my-search-box {
+    width: 100%;
+    height: 36px;
+    background-color: #fff;
+    // border-radius: 18px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .placeholder {
+      font-size: 15px;
+      margin-left: 5px;
+    }
+  }
+}
+</style>
--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,5 +1,8 @@
 <template>
   <view>
+    <!-- 使用自定义的搜索组件 -->
+    <!-- <my-search :bgcolor="'yellow'" :radius="3"></my-search> -->
+    <my-search @click="gotoSearch"></my-search>
     <view class="scroll-view-container">
       <!-- 左侧的滑动区域 -->
       <scroll-view class="left-scroll-view" scroll-y="true" :style="{height: wh + 'px'}">
@@ -44,7 +47,7 @@
     onLoad() {
       const sysInfo = uni.getSystemInfoSync()
       // console.log(sysInfo);
-      this.wh = sysInfo.windowHeight
+      this.wh = sysInfo.windowHeight - 50
       this.getCateList()
     },
     methods: {
@@ -66,6 +69,12 @@
       gotoGoodsList(item) {
         uni.navigateTo({
           url: '/subpkg/goods_list/goods_list?cid=' + item.cat_id
+        })
+      },
+      gotoSearch() {
+        // console.log('ok');
+        uni.navigateTo({
+          url: '/subpkg/search/search'
         })
       }
     }
